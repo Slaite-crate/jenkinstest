491887d23d53cb2d748af22a85e7ff82ee0b1ef9
Pelle <pelle.park@gmail.com>
2020-10-28 14:53:54 +0100

    hello

--- src/main/java/dk/kea/jenkinstest/controller/HomeController.java
+++ src/main/java/dk/kea/jenkinstest/controller/HomeController.java
@@ -1,7 +1,14 @@
 package dk.kea.jenkinstest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
 
-    //hello
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
 }
